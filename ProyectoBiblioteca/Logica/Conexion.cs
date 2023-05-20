926592d0618d381483b14fa3f4f2bb4a91de185b
Dumilde Silva <domilde.silva14@gmail.com>
2023-05-21 00:06:27 +0100

    Biblioteca desenvolvida com ASP.NET no padrão MVC

---
+++ ProyectoBiblioteca/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBiblioteca.Logica
+{
+    public class Conexion
+    {
+        public static string CN = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=DB_BIBLIOTECA_SILVANS;Data Source=DUMILDE\DUMILDE";
+    }
+}
